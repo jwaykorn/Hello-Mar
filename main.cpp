77d22956c52f351c186b8212745c7302f734715f
jwaykorn <39506537+jwaykorn@users.noreply.github.com>
2018-06-01 17:48:33 +0700

    Hello mar ket input number
    get number on keyboard.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	printf("Hello Mar!!");
+	int i=0;
+	printf("Hello Mar!!\n");
+	printf("Enter Your State Number: ");
+	scanf("%d",&i);
+	printf("Stat: %d", i);
 	return 0;
+	
 }
