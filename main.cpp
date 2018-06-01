ddf58b51dfddc9df145a08a93267eb78fa475ad9
jwaykorn <39506537+jwaykorn@users.noreply.github.com>
2018-06-01 17:21:00 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -7,16 +6,3 @@ int main(int argc, char** argv) {
 	printf("Hello Mar!!");
 	return 0;
 }
-=======
-#include <iostream>
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char** argv) {
-	int a = 0;
-	printf("Hello Mar!!");
-	printf("Hello Mar!!");
-	scanf("%d", &a);
-	return 0;
-}
->>>>>>> 82641829b31066ec25b0491de74fea31a626f4dd
